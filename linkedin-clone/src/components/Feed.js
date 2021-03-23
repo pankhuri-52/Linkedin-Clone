188c8460b7afc5397f86ff5fbbbcefcbc884bbbe
Pankhuri Trikha <pankhuritrikha@gmail.com>
2021-03-23 21:10:03 +0530

    Input Designed

--- linkedin-clone/src/components/Feed.js
+++ linkedin-clone/src/components/Feed.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import '../css/Feed.css';
+import InputOption from './InputOption';
 import CreateIcon from '@material-ui/icons/Create';
+import ImageIcon from '@material-ui/icons/Image';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import EventNoteIcon from '@material-ui/icons/EventNote';
+import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay';
 
 function Feed() {
     return (
@@ -12,6 +17,13 @@ function Feed() {
                         <input type="text" />
                         <button type="submit">Send</button>
                     </form>
+                </div>
+
+                <div className="feed__inputOptions">
+                    <InputOption Icon={ImageIcon} title='Photo' color='#70B5F9' />
+                    <InputOption Icon={SubscriptionsIcon} title='Video' color='#E7A33E' />
+                    <InputOption Icon={EventNoteIcon} title='Event' color='#C0CBCD' />
+                    <InputOption Icon={CalendarViewDayIcon} title='Write Article' color='#7FC15E' />
                 </div>
             </div>
         </div>
