0e268ecf4b1c430b346d1ba01dc3336d71c44b01
Pankhuri Trikha <pankhuritrikha@gmail.com>
2021-05-12 17:50:18 +0530

    Front-End ready for Login Form

--- linkedin-clone/src/components/Login.js
+++ linkedin-clone/src/components/Login.js
@@ -1,21 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../css/Login.css';
+import { auth } from './firebase';
 
     function Login() {
 
-        const register = () => {};
+        const [email, setEmail] = useState("");
+        const [password, setPassword] = useState("");
+        const [name, setName] = useState("");
+        const [profilePicture, setProfilePicture] = useState("");
+
+        const LoginToApp = (e) => {
+            // auth
+            e.preventDefault();
+        };
+
+        const register = () => {
+            if (!name) {
+                return alert("You need to enter Full Name");
+            }
+        };
 
         return (
             <div className='login'>
                 <img src="https://1000logos.net/wp-content/uploads/2017/03/Linkedin-Logo.png" />
                 <form>
-                    <input placeholder="Full Name(required if registering)" type="text" />
-                    <input placeholder="Profile picture(optional)" type="text" />     
-                    <input placeholder="Email" type="email" />
-                    <input placeholder="Password" type="password" />
-                    <button>SignIn</button>
+                    <input value={name}
+                           onChange={(e) => setName(e.target.value)}
+                           placeholder="Full Name(required if registering)" 
+                           type="text" />
+
+                    <input value={profilePicture}
+                           onChange={(e) => setProfilePicture(e.target.value)}
+                           placeholder="Profile picture(optional)" 
+                           type="text" />
+
+                    <input value={email} 
+                           onChange={(e) => setEmail(e.target.value)} 
+                           placeholder="Email" 
+                           type="email" />
+
+                    <input value={password}
+                           onChange={(e) => setPassword(e.target.value)} 
+                           placeholder="Password" 
+                           type="password" />
+
+                    <button type='submit' onClick={LoginToApp}>Sign In</button>
                 </form>
-                <p>Not a member? <span className="login__register">Register Now</span></p>
+                <p>Not a member?{" "}<span className="login__register" onClick={register} >Register Now</span></p>
             </div>
         );
     }
